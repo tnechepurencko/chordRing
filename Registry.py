ac2f97d5056cfcac9f012f27817c652c2ce2578a
tatiana <monday240220@gmail.com>
2022-10-09 11:44:24 +0300

    + registry ! make port = sys.argv[1] later

--- Registry.py
+++ Registry.py
@@ -1,3 +1,9 @@
+from concurrent import futures
+
+import grpc
+
+import chord_pb2_grpc as pb2_grpc
+import chord_pb2 as pb2
 import sys
 import random
 
@@ -7,51 +13,73 @@ m = int(sys.argv[2])
 id_ipaddr_port_dict = dict()
 
 
-def register(ipaddr, port):
-    random.seed(0)
-    if len(id_ipaddr_port_dict) < 2 ** m:
-        new_id = random.randint(0, 2 ** m - 1)
-        while new_id in id_ipaddr_port_dict.keys():
+class Registry(pb2_grpc.RegistryServicer):
+    def register(self, request, context):
+        random.seed(0)
+        ipaddr = request.ipaddr
+
+        if len(id_ipaddr_port_dict) < 2 ** m:
             new_id = random.randint(0, 2 ** m - 1)
-        id_ipaddr_port_dict[new_id] = f'{ipaddr}:{port}'
-        return new_id, m
-    return -1, 'Chord is full'
+            while new_id in id_ipaddr_port_dict.keys():
+                new_id = random.randint(0, 2 ** m - 1)
+            id_ipaddr_port_dict[new_id] = f'{ipaddr}:{port}'
+            reply = {'id': new_id, 'm': m}
+        else:
+            reply = {'id': -1, 'error': 'Chord is full'}
+
+        return pb2.RegisterReply(**reply)
+
+    def deregister(self, request, context):
+        node_id = request.id
+
+        if node_id in id_ipaddr_port_dict.keys():
+            id_ipaddr_port_dict.pop(node_id)
+            reply = {'stat': True, 'msg': f'The node {node_id} deregistered successfully'}
+        else:
+            reply = {'stat': False, 'msg': f'The node {node_id} does not exist'}
+
+        return pb2.DeregisterReply(**reply)
+
+    @staticmethod
+    def succ_id(node_id):
+        ids = sorted(list(id_ipaddr_port_dict.keys()))
+        if node_id == ids[-1]:
+            return ids[0]
+        for id in ids:
+            if id > node_id:
+                return id
+
+    @staticmethod
+    def pred_id(node_id):
+        ids = sorted(list(id_ipaddr_port_dict.keys()), reverse=True)
+        if node_id == ids[-1]:
+            return ids[0]
+        for id in ids:
+            if id < node_id:
+                return id
+
+    def populate_finger_table(self, request, context):
+        node_id = request.id
+        finger_table_ids = set(self.succ_id((node_id + 2 ** (i - 1)) % (2 ** m)) for i in range(1, m + 1))
+        finger_table = [{'id': id, 'addr': id_ipaddr_port_dict[id]} for id in finger_table_ids]
+        reply = {'predID': self.pred_id(node_id), 'ft': finger_table}
+        return pb2.PFTReply(**reply)
+
+    def get_chord_info(self, request, context):
+        chord_info = [{'id': key, 'addr': id_ipaddr_port_dict[key]} for key in id_ipaddr_port_dict.keys()]
+        reply = {'ci': chord_info}
+        return pb2.GCIReply(**reply)
 
 
-def deregister(node_id):
-    if node_id in id_ipaddr_port_dict.keys():
-        id_ipaddr_port_dict.pop(node_id)
-        return True, f'The node {node_id} deregistered successfully'
-    return False, f'The node {node_id} does not exist'
+if __name__ == '__main__':
+    # port = sys.argv[1]
+    port = '5555'
 
-
-def succ_id(node_id):
-    ids = sorted(list(id_ipaddr_port_dict.keys()))
-    if node_id == ids[-1]:
-        return ids[0]
-    for id in ids:
-        if id > node_id:
-            return id
-
-
-def pred_id(node_id):
-    ids = sorted(list(id_ipaddr_port_dict.keys()), reverse=True)
-    if node_id == ids[0]:
-        return ids[-1]
-    for id in ids:
-        if id < node_id:
-            return id
-
-
-def populate_finger_table(node_id):
-    finger_table_ids = set(succ_id((node_id + 2 ** (i - 1)) % (2 ** m)) for i in range(1, m + 1))
-    finger_table = [(id, id_ipaddr_port_dict[id]) for id in finger_table_ids]
-    return pred_id(node_id), finger_table
-
-
-def get_chord_info():
-    return [(key, id_ipaddr_port_dict[key]) for key in id_ipaddr_port_dict.keys()]
-
-
-
-
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_RegistryServicer_to_server(Registry(), server)
+    server.add_insecure_port('127.0.0.1:' + port)
+    server.start()
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        print('shutting down')
